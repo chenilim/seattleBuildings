bcce1f0908d3a22bf08b6b59d49447e3e4eec929
Chen-I Lim <chenilim@gmail.com>
2020-12-30 13:42:15 -0800

    group by year

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 let rows
-let items
+let allItems
 
 async function loadData() {
     const url = './rows.json'
     const response = await fetch(url)
     rows = await response.json()
 
-    items = rows.data.map(r => ({
+    allItems = rows.data.map(r => ({
         permitSubClass: r[9],
         permitClass: r[10],
         permitType: r[11],
@@ -17,11 +17,14 @@ async function loadData() {
         status: r[22]
     }))
 
-    console.log(`Loaded ${items.length} rows.`)
+    console.log(`Loaded ${allItems.length} rows.`)
 }
 
 async function main() {
     await loadData()
+
+    const items = allItems.filter(o => o.permitClass === 'Residential' && o.completedDate !== null)
+    console.log(`${items.length} filtered items`)
 
     const dates = items.map(o => o.completedDate)
         .filter(o => o)
@@ -31,6 +34,22 @@ async function main() {
 
     console.log(`minDate: ${minDate}`)
     console.log(`maxDate: ${maxDate}`)
+
+    let yearCounts = {}
+    for (item of items) {
+        const year = item.completedDate.getFullYear()
+        const yearCount = yearCounts[year]
+        if (yearCount) {
+            yearCounts[year] = yearCount + 1
+        } else {
+            yearCounts[year] = 1
+        }
+    }
+
+    for (year of Object.keys(yearCounts)) {
+        const count = yearCounts[year]
+        console.log(`${year}: ${count}`)
+    }
 }
 
 main()

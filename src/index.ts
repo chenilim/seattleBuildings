13fbe7885de7b1800d4ba3a993e34e2f78342e85
Chen-I Lim <chenilim@gmail.com>
2020-12-31 18:45:08 -0800

    Draw map

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,28 @@
 import './main.css'
 import * as Chart from 'chart.js'
 
-let rows: any = []
-let allItems: any[] = []
+let allItems: Row[] = []
 let chart: Chart
+
+type Row = {
+	id: string,
+	permitSubClass: string,
+	permitClass: string,
+	permitType: string,
+	permitSubType: string,
+	description: string,
+	issued: string,
+	expires: string,
+	completed: string,
+	status: string,
+	address: string,
+	city: string,
+	state: string,
+	zip: string,
+	lat: number,
+	long: number,
+	link: string,
+}
 
 async function loadData(): Promise<void> {
 	console.log(`LoadData...`)
@@ -12,7 +31,7 @@ async function loadData(): Promise<void> {
 	// const url = './allrows.json'
 	const response = await fetch(url)
 	console.log(`Loading JSON...`)
-	rows = await response.json()
+	const rows = await response.json()
 
 	allItems = rows
 
@@ -143,7 +162,7 @@ function drawChart(permitClass?: string, subType?: string, status?: string) {
 		items = items.filter(o => o.permitSubType === subType)
 	}
 
-	let dateKey = 'completed'
+	let dateKey: 'completed' | 'issued' = 'completed'
 	if (status) {
 		items = items.filter(o => o.status === status)
 		if (status !== 'Completed') {
@@ -153,9 +172,10 @@ function drawChart(permitClass?: string, subType?: string, status?: string) {
 
 	const title = `${subType} ${permitClass}`
 	createChart(items, title, dateKey)
+	renderMap(items)
 }
 
-function createChart(items: any[], title: string, dateKey = 'completed') {
+function createChart(items: Row[], title: string, dateKey: 'completed' | 'issued' | 'expires' = 'completed') {
 	console.log(`${title}: ${items.length} items`)
 
 	// const dates = items.map((o: any) => new Date(o.completed))
@@ -189,8 +209,8 @@ function createChart(items: any[], title: string, dateKey = 'completed') {
 	// }
 
 	const canvas = document.getElementById('mainCanvas') as HTMLCanvasElement
-	canvas.style.maxWidth = '800px'
-	canvas.style.maxHeight = '500px'
+	canvas.style.width = '800px'
+	canvas.style.height = '500px'
 	const ctx = canvas.getContext('2d')!
 
 	const chartData: Chart.ChartData = {
@@ -283,6 +303,47 @@ function clearLog() {
 function log(msg: string) {
 	const panel = document.getElementById('sidebar') as HTMLDivElement
 	panel.innerText = panel.innerText + msg + '\n'
+}
+
+function renderMap(items: Row[]) {
+	const canvas = document.getElementById('mapCanvas') as HTMLCanvasElement
+	canvas.width = 1600
+	canvas.height = 1000
+	canvas.style.width = '800px'
+	canvas.style.height = '500px'
+	const ctx = canvas.getContext('2d')!
+
+	const data = items.map(o => ({
+		lat: Number(o.lat),
+		long: Number(o.long)
+	})).filter(o => o.lat && o.long)
+
+	const longs = data.map(o => o.long)
+	const minLong = longs.reduce(function (a, b) { return a < b ? a : b })
+	const maxLong = longs.reduce(function (a, b) { return a > b ? a : b })
+	const longSpan = maxLong - minLong
+
+	const lats = data.map(o => o.lat)
+	const minLat = lats.reduce(function (a, b) { return a < b ? a : b })
+	const maxLat = lats.reduce(function (a, b) { return a > b ? a : b })
+	const latSpan = maxLat - minLat
+
+	ctx.clearRect(0, 0, canvas.width, canvas.height)
+
+	const padding = 10
+	const width = canvas.width - padding * 2
+	const height = canvas.height - padding * 2
+
+	ctx.fillStyle = "rgba(0, 128, 192, 0.35)"
+	data.forEach(o => {
+		const x = padding + (o.long - minLong) * width / longSpan
+		const y = padding + (o.lat - minLat) * height / latSpan
+		const size = 3
+
+		ctx.beginPath()
+		ctx.arc(x, y, size, 0, 2 * Math.PI)
+		ctx.fill()
+	})
 }
 
 main()
